779b4f1815fabdff436afbb783906841f7a68f43
Dhaiwat <dhaiwatpandya@gmail.com>
2024-02-03 21:52:06 -0600

    refactor

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: NextRequest): Promise<Response> {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: 'Play again',
+          label: 'Flip Again!',
         },
       ],
       image: `${NEXT_PUBLIC_URL}/${coinToss}.png`,
